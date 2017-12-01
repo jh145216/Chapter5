1a6aa22e2e8a35718c44d20d590d898cea78e4fb
jh145216 <31322578+jh145216@users.noreply.github.com>
2017-12-01 10:14:45 -0600

    update For Next Demo 2 Form

---
+++ For Next Demo 2 Form/For Next Demo 2 Form/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+        Dim intCount As Integer     ' Loop counter
+
+        ' Move the image across the form.
+        For intCount = 16 To 328
+            picMcLarenP1.Left = intCount
+        Next
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
---
+++ For Next Demo 2 Form/For Next Demo 2 Form/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("For Next Demo 2 Form")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Davenport Community School District")> 
+<Assembly: AssemblyProduct("For Next Demo 2 Form")> 
+<Assembly: AssemblyCopyright("Copyright © Davenport Community School District 2017")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("19a9e54b-42bc-4d4b-b93a-eb8e84e00946")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
